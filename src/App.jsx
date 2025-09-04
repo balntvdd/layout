d2f5a3e5a66b13e67cebdd41172e74cfbd8d1e4c
balntvdd <bonatividadjr.student@ua.edu.ph>
2025-09-04 14:17:42 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           <div className="rp">Related Posts</div>
         </div>
       </div>
-      <div className="footer">footer</div>
+      <div className="footer">Footer</div>
     </div>
   );
 }
